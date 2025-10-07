e270b14102bbeb15ad58422a04c85cc3200fadaf
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-07 15:22:23 -0300

    feat: add logger with pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'pino-nestjs';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { UsersModule } from './users/users.module';
         synchronize: true,
       }),
     }),
+    LoggerModule.forRoot(),
     UsersModule,
   ],
   controllers: [AppController],
