318a6cc61417ea453bc774bf517c41615016b501
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-07 16:38:01 -0300

    feat: add create for user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,38 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) throw new ConflictException('Email already exists.');
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const userInstance = this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    const savedUser = await this.usersRepository.save(userInstance);
+
+    return savedUser;
+  }
 
   async findOne(email: string): Promise<User | null> {
     const user = await this.usersRepository.findOne({ where: { email } });
