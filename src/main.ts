d2a0f905f14c99d9db3955736246e36fd6c1053d
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-07 15:41:23 -0300

    feat: add class-validator, class-trasnformer and create global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'pino-nestjs';
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: () => {
+        return new UnprocessableEntityException('Invalid fields.');
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => console.error(error));
