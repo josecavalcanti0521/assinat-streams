30d1327c3f7e828b2e2bb72b3bf2b2944499a535
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-07 16:39:47 -0300

    refactor: remove create of users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.usersService.create(createUserDto);
-  }
 }
