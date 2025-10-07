f8fd8790a1626b58372d217bc58787418e4ba776
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-07 13:57:09 -0300

    feat: add findOne in users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async findOne(email: string): Promise<User | null> {
+    const user = await this.usersRepository.findOne({ where: { email } });
+
+    if (!user) throw new NotFoundException('User not found.');
+
+    return user;
+  }
+}
