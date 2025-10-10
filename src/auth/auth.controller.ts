5d2107c32f61345aecdc69098b5469c45a3f3ddf
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-09 23:23:53 -0300

    feat: add enpoint signup. signin and test in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { signInDto } from './dtos/signin.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  async singup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.singup(createUserDto);
+  }
+
+  @Post('/signin')
+  @HttpCode(HttpStatus.OK)
+  async signin(@Body() signInDto: signInDto) {
+    return await this.authService.signin(signInDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/test')
+  test() {
+    return 'Hello, World!';
+  }
+}
