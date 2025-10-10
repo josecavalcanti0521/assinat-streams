7b346bf749c4337331072740d079bfcd3fdbf58e
José Cavalcanti <josemarcelo0521@gmail.com>
2025-10-09 23:24:18 -0300

    feat: add jwt strategy and local strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AccessTokenPayload } from '../interfaces/access-token-payload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: AccessTokenPayload) {
+    return payload;
+  }
+}
